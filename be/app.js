6c287a9fca89de05d9777a24fc18049dcff55117
rafiq <rafiq.danova@gmail.com>
2024-12-24 17:48:40 +0800

    daftarkan route jadwal

--- be/app.js
+++ be/app.js
@@ -7,8 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dokterRouter = require('./routes/dokter')
-
-var dokterController = require('./controller/dokter.controller')
+var jadwalRouter = require('./routes/jadwal')
 
 var app = express();
 
@@ -25,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/doctors', dokterRouter);
+app.use('/schedules', jadwalRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
